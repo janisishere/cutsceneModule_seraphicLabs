8221f940b61ed9723c67083fdb7ba8f87db6b277
janis <124673251+janisishere@users.noreply.github.com>
2025-04-06 12:06:30 +0200

    Update cutsceneModule_Info.lua

--- cutsceneModule_Info.lua
+++ cutsceneModule_Info.lua
@@ -1,6 +1,10 @@
 local info = {}
 
 info.newestVersion = 1
-info.whatsInNewestUpdate = "\n[VERSION 1]\n Published Module!"
+info.whatsInNewestUpdate = {
+  "[[ VERSION 1 ]]",
+  "- published the module!",
+  "expect updates in the future",
+}
 
 return info
